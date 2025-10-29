8ab4e1cc7da8247a3d10a94beb9f974c18198067
vachagunn <vachikpuper@gmail.com>
2025-10-29 17:57:26 +0500

    start project

---
+++ commands.md
@@ -0,0 +1,7 @@
+# Временнный файл с командами
+
+## Создание проекта с TypeScript
+npx create-react-app {название проекта} --template typescript
+
+## Установка SCSS
+npm install -g sass
--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <title>4 в ряд</title>
+      
+      <div className="game-container">
+        <h1>4 в ряд</h1>
+      </div>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
